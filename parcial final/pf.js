90a55efe988bff4bb69497ef7e1f4ba992bf0036
ingsoftManuel <132092799+ingsoftManuel@users.noreply.github.com>
2023-06-16 11:01:30 -0500

    Add files via upload

---
+++ parcial final/pf.js
@@ -0,0 +1,8 @@
+let Dulces = [
+    {tipo: "chocolatina", relleno: "chocolate", textura: "suave", marca: "jet", color: "cafe"},
+    {tipo: "menta", relleno: "no tiene", textura: "crocante", marca: "halls", color: "transparente"},
+    {tipo: "galleta", relleno: "crema", textura: "crocrante", marca: "oreo", color: "negro"},
+    {tipo: "chicles", relleno: "sabor fresa", textura: "suave", marca: "trident", color: "blanco con rosa"}
+
+];
+console.log (Dulces)
